894d4e762501552baff93c488ac11abd23c32f5b
Sebastian Schaum <ses@crosscan.com>
2013-06-23 22:28:49 +0200

    Fixed syntax errors in manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,9 @@
 # Examples
 #
 #   include quassel
-classe quassel {
-    package { 'quassel':
-        provider => 'appdmg',
-        source   => "http://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_0.9.0.dmg",
-    }
+class quassel {
+  package { 'Quassel':
+    provider => 'appdmg',
+    source   => 'http://quassel-irc.org/pub/QuasselClient_MacOSX-x86_64_0.9.0.dmg',
+  }
 }
