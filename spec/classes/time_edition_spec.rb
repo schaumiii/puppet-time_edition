a4783a647eaab28947f75c2bdea5e65c157210c0
Sebastian Schaum <ses@crosscan.com>
2013-06-25 20:40:38 +0200

    fixed wrong url used in spec file

--- spec/classes/time_edition_spec.rb
+++ spec/classes/time_edition_spec.rb
@@ -4,7 +4,7 @@ describe 'time_edition' do
   it do
     should contain_package('Time Edition').with({
       :provider => 'compressed_app',
-      :source   => 'http://downloads.sourceforge.net/timeedition/timeEdition1.1.6-macosx.dmg?use_mirror=',
+      :source   => 'http://downloads.sourceforge.net/timeedition/timeEdition1.1.6-macosx.dmg.zip?use_mirror=',
     })
   end
 end
